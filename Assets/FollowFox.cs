b5b4a550f73fb797eb89d8470872024d4fae3783
Elliot Hunt <elykuu@gmail.com>
2020-01-28 02:13:54 -0700

    Camera Follows Fox Instead of Pointing At

--- Assets/FollowFox.cs
+++ Assets/FollowFox.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class FollowFox : MonoBehaviour
 {
+    GameObject fox;
     // Start is called before the first frame update
     void Start()
     {
-        
+        fox = GameObject.Find("Fox");
     }
 
     // Update is called once per frame
@@ -18,7 +19,10 @@ public class FollowFox : MonoBehaviour
 
     void FixedUpdate()
     {
-        transform.LookAt((GameObject.Find("Fox")).transform);
+        //transform.LookAt((GameObject.Find("Fox")).transform);
+        transform.position = new Vector3(fox.transform.position.x, fox.transform.position.y+3f, transform.position.z);
+        
+
     }
 
 }
